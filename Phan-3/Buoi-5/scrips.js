6ec4bec940149c4fe7b237e70403de745cc7de46
Jimm9 <jimm9tran@gmail.com>
2024-07-30 17:00:34 +0700

    2

---
+++ Phan-3/Buoi-5/scrips.js
@@ -0,0 +1,50 @@
+// var button = document.querySelector("button");
+//     button.onclick = function(){
+//         alert("Hello World!");
+//     }
+
+
+// var a = 10;
+// var b = 20;
+// var fullname = "jfsdfsdlfs"
+// var c = a + b;
+// var ans = b + " " + fullname
+// console.log(ans);
+
+// var results = prompt("Nhap n:");
+// console.log(results);
+
+// setTimeout(function(){
+//     console.log("5s");
+// }, 500);
+
+// setInterval(function (){
+//     //code
+//     console.log("A");
+// }, 1000);
+
+// var b = setInterval(function (){
+//     console.log("B");
+// }, 1000);
+
+// setTimeout(function(){
+//     clearInterval(b);
+// }, 5000);
+
+
+// setTimeout(function (){
+//     clearInterval(setInterval(function(){
+//         console.log("Jimm9 Tran");
+//     }, 1000));
+// }, 5000);
+
+// var a = 10;
+// var b = "Nothing ..."
+// var c
+// var d = null
+
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(typeof(c));
+// console.log(typeof(d));
+// console.log(typeof a == "number")
