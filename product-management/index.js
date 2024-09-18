6f0272d0df27892ad3bd30b9d5b961f6faa96399
jimm9 Tran <jimm9tran@gmail.com>
2024-09-18 22:05:07 +0700

    update

--- product-management/index.js
+++ product-management/index.js
@@ -3,8 +3,10 @@ require("dotenv").config();
 
 const db = require("./config/database");
 db.connect();
+
 const route = require("./routers/client/index.route");
 const routeAdmin = require("./routers/admin/index.route");
+
 const app = express()
 const port = process.env.PORT;
 
@@ -12,6 +14,10 @@ app.set("views", "./view");
 app.set("view engine", "pug");
 
 app.use(express.static("public"));
+
+// App Locals Variables
+const systemConfig = require("./config/system")
+app.locals.prefixAdmin = systemConfig.prefixAmin;
 
 // Routes
 route(app);
