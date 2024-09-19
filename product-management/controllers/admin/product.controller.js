d6f94400915f9636135b8dd6d6a94037d9696f6d
jimm9 Tran <jimm9tran@gmail.com>
2024-09-19 15:17:42 +0700

    add find by status

--- product-management/controllers/admin/product.controller.js
+++ product-management/controllers/admin/product.controller.js
@@ -2,12 +2,41 @@ const Products = require("../../models/product.model");
 const searchHelper = require("../../helpers/search");
 const paginationHelper = require("../../helpers/pagination");
 
+// [GET] /admin/produts
 module.exports.index = async (req, res) => {
+
+
+    let fillterStatus = [
+        {
+            name: "Tất cả",
+            status: "",
+            class: "active"
+        },
+        {
+            name: "Hoạt động",
+            status: "active",
+            class: ""
+        },
+        {
+            name: "Dừng hoạt động",
+            status: "inactive",
+            class: ""
+        }
+    ]
+
     let find =  {
 
     }
     if (req.query.status){
         find.status = req.query.status;
+    }
+
+    if (req.query.status){
+        const index = fillterStatus.findIndex(iten => item.status = req.query.status);
+        fillterStatus[index].class = "active";
+    }{
+        const index = fillterStatus.findIndex(iten => item.status = "");
+        fillterStatus[index].class = "active";
     }
 
     const objectSearch = searchHelper(req.query);
@@ -39,6 +68,7 @@ module.exports.index = async (req, res) => {
         pageTitle: "Trang Sản Phẩm",
         products: products,
         keyword: objectSearch.keyword,
+        fillterStatus: fillterStatus,
         pagination: objectPagination
     });
 };
--- product-management/public/admin/js/script.js
+++ product-management/public/admin/js/script.js
@@ -1,4 +1,24 @@
+// Buttom Status
 
+const buttonStatus = document.querySelectorAll("[button-status]");
+if (buttonStatus.length>0){
+    let url = new URL(window.location.href);
+
+    buttonStatus.forEach(button => {
+        button.addEventListener('click', () => {
+            const status = button.getAttribute("button-status");
+            
+            if (status){    
+                url.searchParams.set("status", status);
+            }else{
+                url.searchParams.delete("status");
+            }
+
+            window.location.href = url.href
+        })
+    })
+}
+// End Bottom Status
 
 // From Search
 
