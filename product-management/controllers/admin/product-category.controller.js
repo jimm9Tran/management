2776a049d8dbddda0f430b08e129afd8a14cb685
jimm9 Tran <jimm9tran@gmail.com>
2024-10-01 15:08:29 +0700

    quản lý danh mục sản phẩm

--- product-management/controllers/admin/product-category.controller.js
+++ product-management/controllers/admin/product-category.controller.js
@@ -1,6 +1,8 @@
 const ProductCategory = require("../../models/product-category.model");
 const systemConfig = require("../../config/system");
 
+const createTreeHelper = require("../../helpers/createTree");
+
 // [GET] //admin/product-category
 module.exports.index = async (req, res) => {
     let find = {
@@ -9,25 +11,33 @@ module.exports.index = async (req, res) => {
 
     const records = await ProductCategory.find(find);
 
+    const newRecords = createTreeHelper.tree(records);
+
     res.render("admin/pages/products-category/index", {
         pageTitle: "Trang danh mục sản phẩm",
-        records: records
+        records: newRecords
     });
 };
 
 // [GET] //admin/product-category/create
-module.exports.create = (req, res) => {
+module.exports.create = async (req, res) => {
+    let find = {
+        deleted: false
+    };
+
+    const records = await ProductCategory.find(find);
+
+    const newRecords = createTreeHelper.tree(records);
+
     res.render("admin/pages/products-category/create", {
-        pageTitle: "Tạo danh mục"
+        pageTitle: "Trang tạo danh mục sản phẩm",
+        records: newRecords
     });
 };
 
+
 // [POST] //admin/product-category/create
 module.exports.createPost = async (req, res) => {
-    // req.body.price = parseInt(req.body.price);
-    // req.body.discountPercentage = parseInt(req.body.discountPercentage);
-    // req.body.stock = parseInt(req.body.stock);
-
     if (req.body.position == "") {
         const countProducts = await ProductCategory.countDocuments();
         req.body.position = countProducts + 1;
