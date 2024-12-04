af70f5ded3b11df60d2ea3de7e4099badd153245
jimm9 Tran <jimm9tran@gmail.com>
2024-12-04 15:15:11 +0700

    order.model

---
+++ product-management/models/order.model.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+    // user_id: String,
+    cart_id: String,
+
+    userInfor: {
+        fullName: String,
+        phone: String,
+        address: String
+    },
+
+    products: [
+        {
+            product_id: String,
+            price: Number,
+            discountPercentage: Number, 
+            quantity: Number
+        }
+    ],
+
+    deleted: {
+        type: Boolean,
+        default: False
+    },
+    deletedDate: Date,
+}, {
+    timestamps: true
+});
+
+
+const Order = mongoose.model("Order", orderSchema, "orders");
+
+module.exports = Order;
